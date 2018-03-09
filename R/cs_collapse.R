b7fd0363906f09901f4f6cb562a20a920dbac3f5
Christopher Prener <chris.prener@slu.edu>
2018-03-09 05:56:57 -0600

    begin documenting functions

--- R/cs_collapse.R
+++ R/cs_collapse.R
@@ -1,6 +1,22 @@
+#' Combine Months in Year List Object
 #'
+#' @description This function takes a list containing 12 tibbles - one per
+#'    month - that have been validated and collapses them into a single tibble.
+#'    This is preferable for working with the data post validation, but cannot
+#'    be done until there is parity in terms of variable names and classes.
+#'
+#' @usage cs_collapse(.data)
+#'
+#' @param .data A list containing monthly crime data
+#'
+#' @return A tibble containing a year worth of crime data.
+#'
+#' @importFrom dplyr bind_rows
+#'
+#' @export
 cs_collapse <- function(.data){
-  
+
+  # extract each month to an individual object
   jan <- cs_extract_month(.data, month = 1)
   feb <- cs_extract_month(.data, month = 2)
   mar <- cs_extract_month(.data, month = 3)
@@ -13,9 +29,11 @@ cs_collapse <- function(.data){
   oct <- cs_extract_month(.data, month = 10)
   nov <- cs_extract_month(.data, month = 11)
   dec <- cs_extract_month(.data, month = 12)
-  
-  year <- bind_rows(jan, feb, mar, apr, may, june, july, aug, sept, oct, nov, dec)
-  
+
+  # combine each object
+  year <- dplyr::bind_rows(jan, feb, mar, apr, may, june, july, aug, sept, oct, nov, dec)
+
+  # return combined object
   return(year)
-  
+
 }
--- R/cs_extract_month.R
+++ R/cs_extract_month.R
@@ -1,12 +1,31 @@
+#' Extract Month from Year List Object
 #'
+#' @description This function extracts a given month from a list containing 12
+#'    tibbles - one per month - for additional data cleaning prior to collapsing
+#'    the list. Since months are ordered alphabetically in the year list objects,
+#'    this function makes the process of extracting a particular month more
+#'    intuitive.
+#'
+#' @usage cs_extract_month(.data, month)
+#'
+#' @param .data A list containing monthly crime data
+#'
+#' @param month A string name or abbreviation of a month, or its numeric value.
+#'    Acceptable inputs include, for example, "January", "january", "Jan",
+#'    "jan", and 1.
+#'
+#' @return A tibble containing a single month worth of crime data.
+#'
+#' @export
 cs_extract_month <- function(.data, month){
-  
+
+  # identify input month
   if (month == "January" | month == "Jan" | month == "january" | month == "jan" | month == 1){
     val <- 5
   } else if (month == "February" | month == "Feb" | month == "february" | month == "feb" | month == 2){
     val <- 4
   } else if (month == "March" | month == "Mar" | month == "march" | month == "mar" | month == 3){
-    val <- 8 
+    val <- 8
   } else if (month == "April" | month == "Apr" | month == "april" | month == "apr" | month == 4){
     val <- 1
   } else if (month == "May" | month == "may" | month == 5){
@@ -17,7 +36,8 @@ cs_extract_month <- function(.data, month){
     val <- 6
   } else if (month == "August" | month == "Aug" | month == "august" | month == "aug" | month == 8){
     val <- 2
-  } else if (month == "September" | month == "Sept" | month == "Sep" | month == "september" | month == "sept" | month == "sep" | month == 9){
+  } else if (month == "September" | month == "Sept" | month == "Sep" | month == "september"
+             | month == "sept" | month == "sep" | month == 9){
     val <- 12
   } else if (month == "October" | month == "Oct" | month == "october" | month == "oct" | month == 10){
     val <- 11
@@ -25,10 +45,12 @@ cs_extract_month <- function(.data, month){
     val <- 10
   } else if (month == "December" | month == "Dec" | month == "december" | month == "dec" | month == 12){
     val <- 3
+  } else {
+    stop("The given argument for month does not match an acceptible input.")
   }
-  
+
   monthData <- .data[[val]]
-  
+
   return(monthData)
-  
+
 }
