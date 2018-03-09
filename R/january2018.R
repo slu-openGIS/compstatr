fa07acbe176c77a5f0ea2008e0b5bd57f6712847
Christopher Prener <chris.prener@slu.edu>
2018-03-09 10:19:12 -0600

    document example data

---
+++ R/january2018.R
@@ -0,0 +1,23 @@
+#' Crimes in St. Louis, January 2018
+#'
+#' A data set containing all reported crimes in St. Louis, Missouri during January 2018.
+#'
+#' @docType data
+#'
+#' @usage data(january2018)
+#'
+#' @format A tibble with 3825 rows and 20 variables:
+#' \describe{
+#'   \item{Compliant}{complaint record number}
+#'   \item{CodedMonth}{year and month crime reported}
+#'   \item{DateOccur}{date and time of crime}
+#'   \item{FlagCrime}{}
+#'   }
+#'
+#' @source \href{http://www.slmpd.org}{St. Louis Metropolitan Police Department}
+#'
+#' @examples
+#' str(january2018)
+#' head(january2018)
+#'
+"january2018"
--- inst/extdata/January2018.R
+++ inst/extdata/January2018.R
@@ -1,8 +1,9 @@
 # create example data
 
 library(devtools)
+library(dplyr)
 library(readr)
 
-january2018 <- read_csv("inst/extdata/January2018.csv")
+january2018 <- as_tibble(read_csv("inst/extdata/January2018.csv"))
 
-use_data(january2018)
+use_data(january2018, overwrite = TRUE)
