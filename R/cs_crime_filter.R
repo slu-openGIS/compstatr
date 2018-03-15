5a37a8593e67e7bbde362134a39c40a0b4f36d02
Cree Foeller <cree.foeller@slu.edu>
2018-03-15 17:59:50 -0500

    Added the rest of the offenses

--- R/cs_crime_filter.R
+++ R/cs_crime_filter.R
@@ -33,7 +33,7 @@ cs_crime_filter <- function(.data, var, crime){
 
     subsetData <- dplyr::filter(.data, !!var <= 50000)
 
-  } else if (crime == "part 1" | crime == "Part 1"){
+  } else if (crime == "part 1" | crime == "Part 1" | crime == "part I" | crime == "Part I"){
 
     subsetData <- dplyr::filter(.data, !!var <= 90000)
 
@@ -41,9 +41,127 @@ cs_crime_filter <- function(.data, var, crime){
 
     subsetData <- dplyr::filter(.data, !!var >= 10000 & !!var < 20000)
 
+  } else if (crime == "Arson" | crime == "arson" | crime == 2){
+
+    subsetData <- dplyr::filter(.data, !!var >= 20000 & !!var < 30000)
+
+  } else if (crime == "Arson" | crime == "arson" | crime == 3){
+
+    subsetData <- dplyr::filter(.data, !!var >= 30000 & !!var < 40000)
+
+  } else if (crime == "Arson" | crime == "arson" | crime == 4){
+
+    subsetData <- dplyr::filter(.data, !!var >= 40000 & !!var < 50000)
+
+  } else if (crime == "Arson" | crime == "arson" | crime == 5){
+
+    subsetData <- dplyr::filter(.data, !!var >= 50000 & !!var < 60000)
+
+  } else if (crime == "Arson" | crime == "arson" | crime == 6){
+
+    subsetData <- dplyr::filter(.data, !!var >= 60000 & !!var < 70000)
+
+  } else if (crime == "Arson" | crime == "arson" | crime == 7){
+
+    subsetData <- dplyr::filter(.data, !!var >= 70000 & !!var < 80000)
+
+
   } else if (crime == "Arson" | crime == "arson" | crime == 8){
 
     subsetData <- dplyr::filter(.data, !!var >= 80000 & !!var < 90000)
+
+
+  } else if (crime == "part 2" | crime == "Part 2" | crime == "part II" | crime == "Part II"){
+
+    subsetData <- dplyr::filter(.data, !!var >= 90000)
+
+
+  } else if (crime == "Other Assaults" | crime == "other assaults" | crime == 9){
+
+    subsetData <- dplyr::filter(.data, !!var >= 90000 & !!var < 100000)
+
+
+  } else if (crime == "Forgery and Counterfeiting" | crime == "forgery and counterfeiting" | crime == 10){
+
+    subsetData <- dplyr::filter(.data, !!var >= 100000 & !!var < 110000)
+
+  } else if (crime == "Fraud" | crime == "fraud" | crime == 11){
+
+    subsetData <- dplyr::filter(.data, !!var >= 110000 & !!var < 120000)
+
+  } else if (crime == "Embezzlement" | crime == "embezzlement" | crime == 12){
+
+    subsetData <- dplyr::filter(.data, !!var >= 120000 & !!var < 130000)
+
+  } else if (crime == "stolen property" | crime == "Stolen Property" | crime == 13){
+
+    subsetData <- dplyr::filter(.data, !!var >= 130000 & !!var < 140000)
+
+  } else if (crime == "Vandalism" | crime == "vandalism" | crime == 14){
+
+    subsetData <- dplyr::filter(.data, !!var >= 140000 & !!var < 150000)
+
+  } else if (crime == "weapons" | crime == "Weapons" | crime == 15){
+
+    subsetData <- dplyr::filter(.data, !!var >= 150000 & !!var < 160000)
+
+  } else if (crime == "prostitution and commercialized vice" | crime == "Prostitution and Commercialized Vice" | crime == 16){
+
+    subsetData <- dplyr::filter(.data, !!var >= 160000 & !!var < 170000)
+
+  } else if (crime == "sex offenses" | crime == "Sex Offense" | crime == 17){
+
+    subsetData <- dplyr::filter(.data, !!var >= 170000 & !!var < 180000)
+
+  } else if (crime == "drug abuse violations" | crime == "Drug Abuse Violations" | crime == 18){
+
+    subsetData <- dplyr::filter(.data, !!var >= 180000 & !!var < 190000)
+
+  } else if (crime == "Gambling" | crime == "gambling" | crime == 19){
+
+    subsetData <- dplyr::filter(.data, !!var >= 190000 & !!var < 200000)
+
+  } else if (crime == "offenses against the family and children" | crime == "Offense Against the Family and Children" | crime == 20){
+
+    subsetData <- dplyr::filter(.data, !!var >= 200000 & !!var < 210000)
+
+  } else if (crime == "driving under the influence" | crime == "Driving Under the Influence" | crime == 21){
+
+    subsetData <- dplyr::filter(.data, !!var >= 210000 & !!var < 220000)
+
+  } else if (crime == "liquor laws" | crime == "Liquor Laws" | crime == 22){
+
+    subsetData <- dplyr::filter(.data, !!var >= 220000 & !!var < 230000)
+
+  } else if (crime == "drunkenness" | crime == "Drunkenness" | crime == 23){
+
+    subsetData <- dplyr::filter(.data, !!var >= 230000 & !!var < 240000)
+
+  } else if (crime == "disorderly conduct" | crime == "Disorderly Conduct" | crime == 24){
+
+    subsetData <- dplyr::filter(.data, !!var >= 240000 & !!var < 250000)
+
+  } else if (crime == "vagrancy" | crime == "Vagrancy" | crime == 25){
+
+    subsetData <- dplyr::filter(.data, !!var >= 250000 & !!var < 260000)
+
+  } else if (crime == "all other offenses" | crime == "All Other Offenses" | crime == 26){
+
+    subsetData <- dplyr::filter(.data, !!var >= 260000 & !!var < 270000)
+
+  } else if (crime == "suspicion" | crime == "Suspicion" | crime == 27){
+
+    subsetData <- dplyr::filter(.data, !!var >= 270000 & !!var < 280000)
+
+  } else if (crime == "curfew and loitering laws" | crime == "Curfew and Loitering Laws" | crime == 28){
+
+    subsetData <- dplyr::filter(.data, !!var >= 280000 & !!var < 290000)
+
+  } else if (crime == "runaways" | crime == "Runaways" | crime == 29){
+
+    subsetData <- dplyr::filter(.data, !!var >= 290000)
+
+
 
   }
 
