02699f7e0fbe2ae45ec453cb0331d69e049814f7
Christopher Prener <chris.prener@slu.edu>
2018-09-20 19:43:35 -0500

    refactor cs_selectMonth

--- R/cs_standardize.R
+++ R/cs_standardize.R
@@ -172,29 +172,30 @@ cs_std26 <- function(.data){
 cs_selectMonth <- function(month){
 
   if (month == "January" | month == "Jan" | month == "january" | month == "jan" | month == 1){
-    val <- 5
+    val <- "January"
   } else if (month == "February" | month == "Feb" | month == "february" | month == "feb" | month == 2){
-    val <- 4
+    val <- "February"
   } else if (month == "March" | month == "Mar" | month == "march" | month == "mar" | month == 3){
-    val <- 8
+    val <- "March"
   } else if (month == "April" | month == "Apr" | month == "april" | month == "apr" | month == 4){
-    val <- 1
+    val <- "April"
   } else if (month == "May" | month == "may" | month == 5){
-    val <- 9
+    val <- "May"
   } else if (month == "June" | month == "Jun" | month == "june" | month == "jun" | month == 6){
-    val <- 7
+    val <- "June"
   } else if (month == "July" | month == "Jul" | month == "july" | month == "jul" | month == 7){
-    val <- 6
+    val <- "July"
   } else if (month == "August" | month == "Aug" | month == "august" | month == "aug" | month == 8){
-    val <- 2
-  } else if (month == "September" | month == "Sept" | month == "Sep" | month == "september" | month == "sept" | month == "sep" | month == 9){
-    val <- 12
+    val <- "August"
+  } else if (month == "September" | month == "Sept" | month == "Sep" | month == "september"
+             | month == "sept" | month == "sep" | month == 9){
+    val <- "September"
   } else if (month == "October" | month == "Oct" | month == "october" | month == "oct" | month == 10){
-    val <- 11
+    val <- "October"
   } else if (month == "November" | month == "Nov" | month == "november" | month == "nov" | month == 11){
-    val <- 10
+    val <- "November"
   } else if (month == "December" | month == "Dec" | month == "december" | month == "dec" | month == 12){
-    val <- 3
+    val <- "December"
   }
 
   return(val)
