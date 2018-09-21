4f3d7f00953f99195374dcd912288fdefeb79684
Christopher Prener <chris.prener@slu.edu>
2018-09-20 20:17:34 -0500

    refactor cs_collapse

--- R/cs_collapse.R
+++ R/cs_collapse.R
@@ -11,7 +11,9 @@
 #'
 #' @return A tibble containing a year worth of crime data.
 #'
+#' @importFrom dplyr arrange
 #' @importFrom dplyr bind_rows
+#' @importFrom purrr map
 #'
 #' @export
 cs_collapse <- function(.data){
@@ -21,22 +23,10 @@ cs_collapse <- function(.data){
     stop('A existing data frame with data to be seperated must be specified for .data')
   }
 
-  # extract each month to an individual object
-  jan <- cs_extract_month(.data, month = 1)
-  feb <- cs_extract_month(.data, month = 2)
-  mar <- cs_extract_month(.data, month = 3)
-  apr <- cs_extract_month(.data, month = 4)
-  may <- cs_extract_month(.data, month = 5)
-  june <- cs_extract_month(.data, month = 6)
-  july <- cs_extract_month(.data, month = 7)
-  aug <- cs_extract_month(.data, month = 8)
-  sept <- cs_extract_month(.data, month = 9)
-  oct <- cs_extract_month(.data, month = 10)
-  nov <- cs_extract_month(.data, month = 11)
-  dec <- cs_extract_month(.data, month = 12)
-
-  # combine each object
-  year <- dplyr::bind_rows(jan, feb, mar, apr, may, june, july, aug, sept, oct, nov, dec)
+  # extract each month, collapse, and re-order
+  purrr::map(.data, as.data.frame) %>%
+    dplyr::bind_rows() %>%
+    dplyr::arrange(CodedMonth, DateOccur) -> year
 
   # return combined object
   return(year)
