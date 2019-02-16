b55ce876fa69f8a94b4e8f3de4d7e711b8de0439
Christopher Prener <chris.prener@slu.edu>
2019-02-16 16:24:56 -0600

    address R CMD check issues with intersections

--- tests/testthat/test_is_int.R
+++ tests/testthat/test_is_int.R
@@ -6,5 +6,9 @@ test_data <- january2018
 
 #
 
-test_that("input error triggered - no .data", expect_error(cs_is_int( ,ILEADSStreet,new_column), "A existing data frame with data to be seperated must be specified for .data"))
-test_that("input error triggered - no variable specified", expect_error(cs_is_int(test_data,,intersection), "The column containing the data to be separated must be specified for variable"))
+test_that("input error triggered - no .data",
+          expect_error(cs_is_int(var = ILEADSStreet, newVar = new_column),
+                       "A existing data frame with data to be separated must be specified for .data"))
+test_that("input error triggered - no variable specified",
+          expect_error(cs_is_int(test_data, newVar = intersection),
+                       "The column containing the data to be separated must be specified for variable"))
--- tests/testthat/test_parse_int.R
+++ tests/testthat/test_parse_int.R
@@ -6,12 +6,20 @@ test_data <- january2018
 
 #
 
-test_that("input error triggered - no .data", expect_error(cs_parse_int( ,ILEADSStreet,Intersection1,Intersection2), "A existing data frame with data to be seperated must be specified for .data"))
+test_that("input error triggered - no .data",
+          expect_error(cs_parse_int( ,ILEADSStreet,Intersection1,Intersection2),
+                       "A existing data frame with data to be separated must be specified for .data"))
 
-test_that("input error triggered - no var", expect_error(cs_parse_int(test_data,,Intersection1,Intersection2), "The column containing the data to be separated must be specified for variable"))
+test_that("input error triggered - no var",
+          expect_error(cs_parse_int(test_data,,Intersection1,Intersection2),
+                       "The column containing the data to be separated must be specified for variable"))
 
-test_that("input error triggered - no dateVar", expect_error(cs_parse_int( test_data,ILEADSStreet,,Intersection2), "The name of the output column containing a street must be specified for newVar1"))
+test_that("input error triggered - no dateVar",
+          expect_error(cs_parse_int( test_data,ILEADSStreet,,Intersection2),
+                       "The name of the output column containing a street must be specified for newVar1"))
 
-test_that("input error triggered - no timeVar", expect_error(cs_parse_int(test_data ,ILEADSStreet,Intersection1,),"The name of the output column containing a street must be specified for newVar2" ))
+test_that("input error triggered - no timeVar",
+          expect_error(cs_parse_int(test_data ,ILEADSStreet,Intersection1,),
+                       "The name of the output column containing a street must be specified for newVar2" ))
 
 
