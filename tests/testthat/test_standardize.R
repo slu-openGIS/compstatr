7cdbc70f348affeec25e1abb9c78bbd2b0bf13cf
Christopher Prener <chris.prener@slu.edu>
2019-03-20 20:07:01 -0500

    write initial tests for cs_standardize

--- tests/testthat/test_standardize.R
+++ tests/testthat/test_standardize.R
@@ -1,17 +1,49 @@
 context("test cs_standardize function")
 
-# load test data
+# load data ------------------------------------------------
 
-test_data <- january2018
+load(system.file("testdata", "yearList13.rda", package = "compstatr", mustWork = TRUE))
+load(system.file("testdata", "yearList17.rda", package = "compstatr", mustWork = TRUE))
 
-#
+# extract data ------------------------------------------------
 
-test_that("input error triggered - no .data", expect_error(cs_standardize(,Feb, 18), "A existing data frame with data to be separated must be specified for .data"))
+test_that("correctly specified functions execute without error", {
+  expect_equal(ncol(cs_extract_month(yearList13, month = "May")), 18)
+  expect_equal(ncol(cs_extract_month(yearList17, month = "May")), 26)
+})
 
-test_that("input error triggered - no month", expect_error(cs_standardize(test_data,,18), "The column containing the data to be separated must be specified for month"))
+# test function ------------------------------------------------
 
-test_that("input error triggered - no config", expect_error(cs_standardize(test_data,Feb,), "The non-standard configuration, either 18 or 26 must be specified"))
+test_that("correctly specified functions execute without error", {
+  expect_error(cs_standardize(yearList13, month = 1, config = 18), NA)
+  expect_error(cs_standardize(yearList13, month = "FEB", config = 18), NA)
+  expect_error(cs_standardize(yearList13, month = "March", config = 18), NA)
+  expect_error(cs_standardize(yearList13, month = "apr", config = 18), NA)
+  expect_error(cs_standardize(yearList13, month = 8, config = 18), NA)
+  expect_error(cs_standardize(yearList17, month = "May", config = 26), NA)
+})
 
-test_that("input error triggered - not recognized config", expect_error(cs_standardize(test_data,Feb,19), "The given argument for month does not match an acceptible input of 18 or 26."))
+# test inputs ------------------------------------------------
 
-test_that("input error triggered - nor recognized month", expect_error(cs_standardize(test_data,brick,18), "The given argument for month does not match an acceptible input."))
+test_that("misspecified functions return errors", {
+  expect_error(cs_standardize(month = "May", config = 18),
+               "A existing year-list object must be specified for .data.")
+  expect_error(cs_standardize(yearList13, config = 18),
+               "The month to be standardized must be specified.")
+  expect_error(cs_standardize(yearList13, month = "ham", config = 18),
+               "The given argument for month does not match an acceptible input.")
+  expect_error(cs_standardize(yearList13, month = 5),
+               "The non-standard configuration, either 18 or 26 must be specified.")
+  expect_error(cs_standardize(yearList13, month = 5, config = 85),
+               "The given argument for 'config' does not match an acceptible input of '18' or '26'.")
+})
+
+# test results ------------------------------------------------
+
+yearList13 <- cs_standardize(yearList13, month = "May", config = 18)
+yearList17 <- cs_standardize(yearList17, month = "May", config = 26)
+
+test_that("column numbers are corrected", {
+  expect_equal(ncol(cs_extract_month(yearList13, month = "May")), 20)
+  expect_equal(ncol(cs_extract_month(yearList17, month = "May")), 20)
+})
