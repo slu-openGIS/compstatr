d65d63d187c7682d0d13148f2e11a4f1e319d4db
Christopher Prener <chris.prener@slu.edu>
2019-03-20 19:47:47 -0500

    unit tests for extract month, refactor

--- R/cs_extract_month.R
+++ R/cs_extract_month.R
@@ -26,7 +26,7 @@ cs_extract_month <- function(.data, month){
 
   # check for missing parameters
   if (missing(.data)) {
-    stop('A existing data frame with data to be seperated must be specified for .data')
+    stop('A existing year-list object must be specified for .data.')
   }
 
   if (missing(month)) {
@@ -34,33 +34,35 @@ cs_extract_month <- function(.data, month){
   }
 
   #quote input variables
-month <- rlang::quo_name(rlang::enquo(month))
+  month <- rlang::quo_name(rlang::enquo(month))
+
+  # convert to lower case
+  month <- tolower(month)
 
   # identify input month
-  if (month == "January" | month == "Jan" | month == "january" | month == "jan" | month == 1){
+  if (month == "january" | month == "jan" | month == 1){
     val <- "January"
-  } else if (month == "February" | month == "Feb" | month == "february" | month == "feb" | month == 2){
+  } else if (month == "february" | month == "feb" | month == 2){
     val <- "February"
-  } else if (month == "March" | month == "Mar" | month == "march" | month == "mar" | month == 3){
+  } else if (month == "march" | month == "mar" | month == 3){
     val <- "March"
-  } else if (month == "April" | month == "Apr" | month == "april" | month == "apr" | month == 4){
+  } else if (month == "april" | month == "apr" | month == 4){
     val <- "April"
-  } else if (month == "May" | month == "may" | month == 5){
+  } else if (month == "may" | month == 5){
     val <- "May"
-  } else if (month == "June" | month == "Jun" | month == "june" | month == "jun" | month == 6){
+  } else if (month == "june" | month == "jun" | month == 6){
     val <- "June"
-  } else if (month == "July" | month == "Jul" | month == "july" | month == "jul" | month == 7){
+  } else if (month == "july" | month == "jul" | month == 7){
     val <- "July"
-  } else if (month == "August" | month == "Aug" | month == "august" | month == "aug" | month == 8){
+  } else if (month == "august" | month == "aug" | month == 8){
     val <- "August"
-  } else if (month == "September" | month == "Sept" | month == "Sep" | month == "september"
-             | month == "sept" | month == "sep" | month == 9){
+  } else if (month == "september" | month == "sept" | month == "sep" | month == 9){
     val <- "September"
-  } else if (month == "October" | month == "Oct" | month == "october" | month == "oct" | month == 10){
+  } else if (month == "october" | month == "oct" | month == 10){
     val <- "October"
-  } else if (month == "November" | month == "Nov" | month == "november" | month == "nov" | month == 11){
+  } else if (month == "november" | month == "nov" | month == 11){
     val <- "November"
-  } else if (month == "December" | month == "Dec" | month == "december" | month == "dec" | month == 12){
+  } else if (month == "december" | month == "dec" | month == 12){
     val <- "December"
   } else {
 
--- tests/testthat/test_extract_month.R
+++ tests/testthat/test_extract_month.R
@@ -1,16 +1,48 @@
-context("test cs_extract_month function")
+context("test cs_extract_month")
 
-# load test data
+# load data ------------------------------------------------
 
-test_data <- january2018
+load(system.file("testdata", "yearList13.rda", package = "compstatr", mustWork = TRUE))
 
-#
+# test function ------------------------------------------------
 
-test_that("input error triggered - no .data", expect_error(cs_extract_month( ,jan), "A existing data frame with data to be seperated must be specified for .data"))
+test_that("correctly specified functions execute without error", {
+  expect_error(cs_extract_month(yearList13, month = 1), NA)
+  expect_error(cs_extract_month(yearList13, month = "MARCH"), NA)
+  expect_error(cs_extract_month(yearList13, month = "april"), NA)
+  expect_error(cs_extract_month(yearList13, month = "May"), NA)
+  expect_error(cs_extract_month(yearList13, month = "jun"), NA)
+  expect_error(cs_extract_month(yearList13, month = 7), NA)
+  expect_error(cs_extract_month(yearList13, month = "8"), NA)
+  expect_error(cs_extract_month(yearList13, month = "SEP"), NA)
+  expect_error(cs_extract_month(yearList13, month = "Nov"), NA)
+  expect_error(cs_extract_month(yearList13, month = "dec"), NA)
+})
 
-test_that("input error triggered - no month", expect_error(cs_extract_month(test_data,), "The month to be extracted must be specified."))
+# test inputs ------------------------------------------------
 
-test_that("input error triggered - not recognized month",expect_error(cs_extract_month(test_data,brick), "The given argument for month does not match an acceptible input."))
+test_that("misspecified functions return errors", {
+  expect_error(cs_extract_month(month = "May"),
+               "A existing year-list object must be specified for .data.")
+  expect_error(cs_extract_month(yearList13),
+               "The month to be extracted must be specified.")
+  expect_error(cs_extract_month(yearList13, month = "ham"),
+               "The given argument for month does not match an acceptible input.")
+})
 
+# test results ------------------------------------------------
 
+result1 <- cs_extract_month(yearList13, month = "Feb")
+
+test_that("data is extracted correctly", {
+  expect_equal("tbl_df" %in% class(result1), TRUE)
+  expect_equal(result1$MonthReportedtoMSHP[1], "2013-02")
+})
+
+result2 <- cs_extract_month(yearList13, month = 10)
+
+test_that("data is extracted correctly", {
+  expect_equal("tbl_df" %in% class(result2), TRUE)
+  expect_equal(result2$CodedMonth[1], "2013-10")
+})
 
